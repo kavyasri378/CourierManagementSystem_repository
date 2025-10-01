1447e7bd43e4ff3bd201db59e5a86ab27800c403
kavyasri <djkavyasri.1318@gmail.com>
2025-10-01 19:58:05 +0530

    courier class get commit after update status function

--- cms/src/CourierService.java
+++ cms/src/CourierService.java
@@ -21,4 +21,24 @@ public class CourierService {
             System.out.println("Error: "+e);
         }
     }
+
+    public void updateStatus(int id,String status){
+        String query="UPDATE courier SET statuc=? WHERE courier_id=?";
+        try(Connection con=DBManager.getConnection();
+        PreparedStatement pmt=con.prepareStatement(query))
+        {
+            pmt.setString(1,status);
+            pmt.setInt(2,id);
+            int r=pmt.executeUpdate();
+            if(r>0){
+                System.out.println("Status of the courier get updated!!");
+            }
+            else{
+                System.out.println("Courier not found");
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error: "+e);
+        }
+    }
 }
