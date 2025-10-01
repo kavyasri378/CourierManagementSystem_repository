9c4f7826f1743e4b7cc1592ec92d273bdd9cf327
kavyasri <djkavyasri.1318@gmail.com>
2025-10-01 13:35:25 +0530

    commit of second version of connection

--- cms/src/DBManager.java
+++ cms/src/DBManager.java
@@ -2,7 +2,13 @@ import java.sql.*;
 
 public class DBManager {
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/cdb";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Load the MySQL driver
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/cdb?useSSL=false&serverTimezone=UTC";
         String user = "root";
         String pwd = "Kavya@2006Kavya";
         return DriverManager.getConnection(url, user, pwd);
