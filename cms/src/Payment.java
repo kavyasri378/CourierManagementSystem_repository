44d3dd3f5e257bebe728b03f9e8c53f8be34b9b5
kavyasri <djkavyasri.1318@gmail.com>
2025-10-01 20:01:16 +0530

    payment status function get commiting

--- cms/src/Payment.java
+++ cms/src/Payment.java
@@ -21,4 +21,24 @@ public class Payment {
         System.out.println("Error: "+e);
        }
     }
+
+    public void updatepaymentStatus(int id,String status){
+        String query="UPDATE payment SET status=? WHERE payment_id=?";
+        try(Connection con=DBManager.getConnection();
+        PreparedStatement pmt=con.prepareStatement(query))
+        {
+            pmt.setString(1,status);
+            pmt.setInt(2,id);
+            int r=pmt.executeUpdate();
+            if(r>0){
+                System.out.println("Status of the payment get updated!!");
+            }
+            else{
+                System.out.println("Courier not found");
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error: "+e);
+        }
+    }
 }
