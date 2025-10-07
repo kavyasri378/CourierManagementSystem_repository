775ba7eecc0360e8a0c3d2daf5d57f7595c19111
kavyasri <djkavyasri.1318@gmail.com>
2025-10-07 19:36:39 +0530

    commit of connection file after made the correct connection

--- cms/src/DBManager.java
+++ cms/src/DBManager.java
@@ -7,7 +7,7 @@ public class DBManager {
         } catch (ClassNotFoundException e) {
             throw new SQLException("MySQL JDBC Driver not found", e);
         }
-
+        // detail to connect
         String url = "jdbc:mysql://localhost:3306/cdb?useSSL=false&serverTimezone=UTC";
         String user = "root";
         String pwd = "Kavya@2006Kavya";
